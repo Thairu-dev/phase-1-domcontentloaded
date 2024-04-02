aa2dae6590b31311c38991f5c8edc854b1d2c176
Joseph_Thairu <joejoement@gmail.com>
2024-04-02 11:56:27 +0300

    completed

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    const thisReallyCool = document.getElementById("text")
+    thisReallyCool.textContent = "This is really cool!"
+  });
